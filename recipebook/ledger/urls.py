e7860b0763b84ef8a58894f86c41635bb81878c9
Martina <martinallamas03@gmail.com>
2026-02-09 21:47:26 +0800

    Add recipe2 view and update list.html format

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import recipe_list, recipe_1
+from .views import recipe_list, recipe_1, recipe_2
 
 urlpatterns = [
     path('recipes/list', recipe_list, name="index"), 
     path('recipe/1', recipe_1, name="recipe_1"),
+    path('recipe/2', recipe_2, name="recipe_2"),
 ]
 
 app_name = "ledger"
