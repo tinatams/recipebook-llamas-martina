535465e327c5673242051876d3756dede231d0a8
Martina <martinallamas03@gmail.com>
2026-02-19 16:42:08 +0800

    Add Ingredient, Recipe, RecipeIngredient models to models.py

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Ingredient(models.Model): 
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('ingredients:ingredient_detail', args=[str(self.id)])
+    
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('recipes:recipe_detail', args=[str(self.id)])
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=50)
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name='recipe_ingredients'
+    )
+
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='recipe_ingredients'
+    )
