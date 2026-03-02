1d37841f84d8bb0713dae7102f4ad7d3bc29039e
Martina <martinallamas03@gmail.com>
2026-03-02 17:10:43 +0800

    Implement redirecting

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+LOGIN_REDIRECT_URL = "/recipes/list"
+LOGOUT_REDIRECT_URL = "/accounts/login"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
