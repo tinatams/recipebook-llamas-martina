1ae66dd0674e7e27cc080f16f08655160ee67a31
Martina <martinallamas03@gmail.com>
2026-03-02 21:53:14 +0800

    Fix formatting

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    shortBio = models.TextField()
+    short_bio = models.TextField()
 
     def __str__(self):
         return self.name
@@ -27,8 +27,8 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         Profile, on_delete=models.CASCADE, related_name="recipes"
     )
-    createdOn = models.DateTimeField(auto_now_add=True)
-    updatedOn = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
