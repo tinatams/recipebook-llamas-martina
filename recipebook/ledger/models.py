4c2f782859639f69c9eac7708713799bbc74f693
Martina <martinallamas03@gmail.com>
2026-03-02 14:47:47 +0800

    Create Profile model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,5 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    shortBio = models.TextField(blank=True)
 
 
 class Ingredient(models.Model):
